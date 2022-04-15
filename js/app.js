0578c782d0f56e9d6ec864f1a535fab8ee96dccd
farangpal <98533096+farangpal@users.noreply.github.com>
2022-04-15 10:17:50 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -119,7 +119,7 @@ connectBtn.addEventListener('click', async () => {
     checkoutBtn.addEventListener('click', async () => {
       statusp.innerText = 'Minting in progress....'
       // paymentAddress is where funds will be send to
-      const paymentAddress = '0x83549FCD8ed149302cFfcfEA193160D0e99ce5c0'
+      const paymentAddress = '0x0298Df47618d3E4f8B98aB1904D6639C47cde10F'
       let totalEth = totalETHSpan.innerText;
       //totalEth = totalEth.toString();
       accounts = await ethereum.request({ method: "eth_requestAccounts" }); //  [Wikipedia](https://en.wikipedia.org/)
